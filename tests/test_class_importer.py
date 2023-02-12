c013f06fc6c1a4aa0910cee187c44e2247a0efd7
Jon Falkenstein <jfalkenstein@trueaccord.com>
2023-02-12 12:32:23 -0600

    bringing over class importer fixes

--- tests/test_class_importer.py
+++ tests/test_class_importer.py
@@ -8,17 +8,16 @@ from sceptre_cdk_handler.class_importer import ClassImporter
 
 class TestClassImporter(TestCase):
     def test_import_class__imports_class_from_referenced_file(self):
-        filepath = Path(__file__).parent / 'assets' / 'file_to_import.py'
+        filepath = Path(__file__).parent / 'assets' / 'parent_dir' / 'inner_dir' / 'class_dir' / 'file_to_import.py'
         class_name = "MyFancyClassToImport"
         importer = ClassImporter()
         result = importer.import_class(filepath, class_name)
-        self.assertEqual(
-            'Success!',
-            result.attribute
-        )
+        self.assertEqual('Success!', result.attribute)
+        self.assertEqual('Success!', result.full_path_import)
+        self.assertEqual('Success!', result.relative_path_import)
 
     def test_import_class__named_class_isnt_on_module__raises_sceptre_exception(self):
-        filepath = Path(__file__).parent / 'assets' / 'file_to_import.py'
+        filepath = Path(__file__).parent / 'assets' / 'parent_dir' / 'inner_dir' / 'class_dir' / 'file_to_import.py'
         importer = ClassImporter()
         with self.assertRaises(SceptreException):
             importer.import_class(filepath, "CantFindMe")
